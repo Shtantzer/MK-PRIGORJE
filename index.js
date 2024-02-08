30f98a2539a11fe482cccc4f96d7e19c1b1b0c49
unknown <seke.lovro@gmail.com>
2024-02-08 14:04:03 +0100

    Adding a MK Prigorje sticker logo and the navigation bar which is responsive on mobile using hamburger menu

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("proba");
+const hamburger = document.querySelector(".hamburger");
+const izbornik = document.querySelector(".izbornik");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    izbornik.classList.toggle("active");
+})
