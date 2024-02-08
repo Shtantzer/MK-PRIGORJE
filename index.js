d5f585ba74d03a30b827c2feeddca35f64e6ac49
Shtantzer <seke.lovro@gmail.com>
2024-02-08 21:02:11 +0100

    Adding a 2015 winter party video, adding some space on footer and doing some color desinging

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector(".hamburger");
 const izbornik = document.querySelector(".izbornik");
 
+
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     izbornik.classList.toggle("active");
