d58b41d448eaec16aec1fad45e3583b5221e7f98
unknown <seke.lovro@gmail.com>
2024-02-09 15:21:17 +0100

    Afer I've added the location of MK Prigorje clubhouse, now I'm adding Events, Contact and our Social media. Also I'm implementing a click event listener for mobile navbar responsiveness

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const hamburger = document.querySelector(".hamburger");
 const izbornik = document.querySelector(".izbornik");
+const gumbi = document.querySelectorAll("li");
 
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     izbornik.classList.toggle("active");
 })
+
+for (var i=0; i<gumbi.length; i++) {
+    gumbi[i].addEventListener("click", () => {
+        hamburger.classList.toggle("active");
+        izbornik.classList.toggle("active"); 
+    });
+}
+
